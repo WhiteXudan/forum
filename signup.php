bf1fe3d1154fcb6c9d7b52f9d791f6610822a776
WhiteXudan <maximetresorclaud15@gmail.com>
2024-06-28 20:05:40 +0000

    SUCCESS

---
+++ signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("includes/head.php"); ?>
+
+<body>
+    <br><br>
+    <form class="container" action="" method="post">
+        <div class="mb-3">
+            <label for="pseudo" class="form-label">Pseudo</label>
+            <input type="text" class="form-control" id="pseudo" name="pseudo">
+        </div>
+        <div class="mb-3">
+            <label for="nom" class="form-label">Nom</label>
+            <input type="text" class="form-control" id="nom" name="lastname">
+        </div>
+        <div class="mb-3">
+            <label for="prenom" class="form-label">Prénom</label>
+            <input type="text" class="form-control" id="prenom" name="firstname">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary">S'inscrire</button>
+    </form>
+</body>
+
+</html>
